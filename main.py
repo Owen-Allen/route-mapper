dc4f6818c15d95a601c30822b10d1777f48aa15d
Marco Borracci <bormar08@outlook.com>
2022-02-26 21:52:01 -0500

    added main file that we can use to construct the network and run social network theory stuff on

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+# to see if bus has an alternate route we need to run a shortest path (dijkstra) algorithm to see if its on the shortest path or not (Nash Equilibrium)
+class Node:
+    adj_list = {}
+
+
+def construct_graph():
+    print("test")
+
+
+if __name__ == '__main__':
+    construct_graph()
