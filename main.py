4b5a3c920a8862e983abe0f6667c25695a0c3268
Marco Borracci <bormar08@outlook.com>
2022-03-02 11:39:16 -0500

    added cost graph

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
 import networkx as nx
+import numpy as np
 from matplotlib import pyplot as plt
 from Bus import Bus
 from Node import Node
@@ -27,15 +27,18 @@ def print_path(path):
 
 def construct_graph():
     graph = Graph()
+    busses = []
     nodeA = Node('A')
     nodeB = Node('B')
     nodeC = Node('C')
     nodeD = Node('D')
     nodeE = Node('E')
 
-    bus1 = Bus("first")
+    bus1 = Bus("Bus 1")
     bus1.set_path([nodeA, nodeB, nodeC, nodeE])
-    # bus1.set_path([nodeA, nodeD, nodeE])
+
+    bus2 = Bus("Bus 2")
+    bus2.set_path([nodeA, nodeD, nodeE])
 
     nodeA.add_edge(nodeB, 10)
     nodeA.add_edge(nodeD, 7)
@@ -51,36 +54,65 @@ def construct_graph():
     graph.add_node(nodeC)
     graph.add_node(nodeD)
     graph.add_node(nodeE)
-    new_path = find_shortest_path(graph, bus1)
-
-    bus1.set_total_travel_time(calculate_cost_of_path(bus1.path))
-
-    if new_path == bus1.path:
-        print("bus1 is on shortest path thus this bus satisfies Nash Equilibrium")
-    else:
-        print("bus1 is not on shortest path thus this bus does not satisfy Nash Equilibrium")
-        print("Alternate route is: ")
-        print_path(new_path)
-        new_cost = calculate_cost_of_path(new_path)
-        print("The new cost is " + str(new_cost) + " which is smaller than the original " + str(
-            bus1.get_total_travel_time()))
-    return graph
+    busses.append(bus1)
+    busses.append(bus2)
+    return graph, busses
 
 
-def display_graph():
+def display_graph(graph):
     graph_display = nx.DiGraph()
-    graph = construct_graph()
     edges = graph.get_all_graph_edges_with_weight()
 
     # add edges to graph
     graph_display.add_weighted_edges_from(edges)
 
-    pos = nx.spring_layout(graph_display)
+    # node layout
+    # pos = nx.spring_layout(graph_display)
+    pos = nx.planar_layout(graph_display)
+
     nx.draw(graph_display, pos, with_labels=True, font_weight='bold')
     edge_weight = nx.get_edge_attributes(graph_display, 'weight')
     nx.draw_networkx_edge_labels(graph_display, pos, edge_labels=edge_weight)
     plt.show()
 
 
+
+
+def display_new_cost_graph(graph, busses):
+    x = np.arange(len(busses))
+    y1 = []
+    y2 = []
+    names = []
+    for bus in busses:
+        current_cost = calculate_cost_of_path(bus.path)
+        bus.set_total_travel_time(current_cost)
+        new_path = find_shortest_path(graph, bus)
+        new_cost = calculate_cost_of_path(new_path)
+        y1.append(current_cost)
+        y2.append(new_cost)
+        names.append(bus.name)
+
+    width = 0.35
+
+    figure, plot = plt.subplots()
+
+    # plot data in grouped manner of bar type
+    current_plot = plot.bar(x - width/2, y1, width, label='Current path')
+    shortest_plot = plot.bar(x + width/2, y2, width, label='Shortest path')
+
+    plot.set_ylabel('Cost')
+    plot.set_xlabel('Busses')
+    plot.set_title('Travel Cost for each bus')
+    plot.set_xticks(x, names)
+    plot.legend()
+
+    plot.bar_label(current_plot)
+    plot.bar_label(shortest_plot)
+
+    plt.show()
+
+
 if __name__ == '__main__':
-    display_graph()
+    graph, busses = construct_graph()
+    display_graph(graph)
+    display_new_cost_graph(graph, busses)
